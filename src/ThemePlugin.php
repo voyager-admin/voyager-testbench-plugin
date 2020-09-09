8a19f599567ef37b52e068de3a9bb18c5ffdd3ae
Christoph Schweppe <info@cschweppe.de>
2020-09-10 01:27:01 +0200

    Update ThemePlugin.php

--- src/ThemePlugin.php
+++ src/ThemePlugin.php
@@ -2,7 +2,6 @@
 
 namespace Voyager\TestbenchPlugin;
 
-use Illuminate\Support\Facades\Route;
 use Voyager\Admin\Contracts\Plugins\ThemePlugin as ThemeBase;
 
 class ThemePlugin implements ThemeBase
@@ -13,21 +12,8 @@ class ThemePlugin implements ThemeBase
     public $website = 'https://github.com/voyager-admin/voyager-testbench-plugin';
     public $version = '1.0.0';
 
-    public function providePublicRoutes(): void
-    {
-        \Log::info('Register routes');
-        Route::get('theme-plugin.css', function () {
-            $response = response('body { background-color: red; }', 200, ['Content-Type' => 'text/css']);
-            $response->setSharedMaxAge(31536000);
-            $response->setMaxAge(31536000);
-            $response->setExpires(new \DateTime('+1 year'));
-
-            return $response;
-        })->name('theme-plugin');
-    }
-
     public function provideCSS(): string
     {
-        return route('voyager.theme-plugin');
+        return 'body { background-color: red; }';
     }
 }
